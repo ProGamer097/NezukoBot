7e57ec5dafa3be65e9afe6897f5b813ca0f517bb
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 19:53:53 +0200

    Update approve.py

--- Nezuko/plugins/bot/approve.py
+++ Nezuko/plugins/bot/approve.py
@@ -1,4 +1,4 @@
-from DAXXMUSIC import app
+from Nezuko import app
 from os import environ
 from config import BOT_USERNAME
 import config
@@ -52,8 +52,8 @@ async def get_userinfo_img(
 
 # --------------------------------------------------------------------------------- #
 
-bg_path = "DAXXMUSIC/assets/userinfo.png"
-font_path = "DAXXMUSIC/assets/hiroko.ttf"
+bg_path = "Nezuko/assets/userinfo.png"
+font_path = "Nezuko/assets/hiroko.ttf"
 
 # --------------------------------------------------------------------------------- #
 
