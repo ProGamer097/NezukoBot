07204ac308ec1678aef60a18086914a594acd321
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 20:51:07 +0200

    Update logger.py

--- Nezuko/utils/logger.py
+++ Nezuko/utils/logger.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import ParseMode
 
-from DAXXMUSIC import app
-from DAXXMUSIC.utils.database import is_on_off
+from Nezuko import app
+from Nezuko.utils.database import is_on_off
 from config import LOGGER_ID
 
 
