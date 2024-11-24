300bab2806fe5bbf9323f672463e6b6819981dca
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 20:05:50 +0200

    Update autoend.py

--- Nezuko/plugins/sudo/autoend.py
+++ Nezuko/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import autoend_off, autoend_on
+from Nezuko import app
+from Nezuko.misc import SUDOERS
+from Nezuko.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
