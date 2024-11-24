6abdaf2e8138d4eb87856cd4275d9d4a1025bf0a
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 21:50:42 +0200

    Update pause.py

--- Nezuko/plugins/admins/pause.py
+++ Nezuko/plugins/admins/pause.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from Nezuko import app
-from Nezuko.core.call import DAXX
+from Nezuko.core.call import hunter
 from Nezuko.utils.database import is_music_playing, music_off
 from Nezuko.utils.decorators import AdminRightsCheck
 from Nezuko.utils.inline import close_markup
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await DAXX.pause_stream(chat_id)
+    await hunter.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
