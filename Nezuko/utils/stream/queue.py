5bf7a1d1ce3a44159cce0b11aaceb7d3db46246c
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 21:42:30 +0200

    Update queue.py

--- Nezuko/utils/stream/queue.py
+++ Nezuko/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from DAXXMUSIC.misc import db
-from DAXXMUSIC.utils.formatters import check_duration, seconds_to_min
+from Nezuko.misc import db
+from Nezuko.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
