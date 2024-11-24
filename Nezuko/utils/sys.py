69f946130da5afaab5808458fceaf2b9ccb9e50a
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 20:50:32 +0200

    Update sys.py

--- Nezuko/utils/sys.py
+++ Nezuko/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from DAXXMUSIC.misc import _boot_
-from DAXXMUSIC.utils.formatters import get_readable_time
+from Nezuko.misc import _boot_
+from Nezuko.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
