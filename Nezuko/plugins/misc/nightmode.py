4489ce9d42ee211ad1a9a57e53985e235070fbf6
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 20:00:10 +0200

    Update nightmode.py

--- Nezuko/plugins/misc/nightmode.py
+++ Nezuko/plugins/misc/nightmode.py
@@ -1,10 +1,10 @@
 import random 
 from pyrogram import filters,Client,enums
-from DAXXMUSIC import app
+from Nezuko import app
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pyrogram.types import ChatPermissions
-from DAXXMUSIC.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
+from Nezuko.mongo.nightmodedb import nightdb,nightmode_on,nightmode_off,get_nightchats 
 
 
 
