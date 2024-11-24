081e03a970b30f9e2c61acd31860157b00c73bd9
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 19:45:16 +0200

    Update wishcute.py

--- Nezuko/plugins/Yumi/wishcute.py
+++ Nezuko/plugins/Yumi/wishcute.py
@@ -2,9 +2,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import random
 import requests
-from DAXXMUSIC import app 
+from Nezuko import app 
 
-SUPPORT_CHAT = "naruto_support1"
+SUPPORT_CHAT = "HunterXsupport"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
