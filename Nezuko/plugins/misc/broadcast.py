5725f8b2197eb1c186d7b8682feba95ac11f4a44
HUNTER_KARAN <KUMAR663959@GMAIL.COM>
2024-11-24 19:57:57 +0200

    Update broadcast.py

--- Nezuko/plugins/misc/broadcast.py
+++ Nezuko/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from DAXXMUSIC import app
-from DAXXMUSIC.misc import SUDOERS
-from DAXXMUSIC.utils.database import (
+from Nezuko import app
+from Nezuko.misc import SUDOERS
+from Nezuko.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from DAXXMUSIC.utils.decorators.language import language
-from DAXXMUSIC.utils.formatters import alpha_to_int
+from Nezuko.utils.decorators.language import language
+from Nezuko.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
