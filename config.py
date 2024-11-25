d60c4d263cc5f88d83ba9a8e6d7f104a67a18fc2
ProGamer097 <156319601+ProGamer097@users.noreply.github.com>
2024-11-25 16:06:12 +0530

    Update config.py
    Signed-off-by: ProGamer097 <156319601+ProGamer097@users.noreply.github.com>

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/hunter-karna/Nezuko.git",
+    "https://github.com/ProGamer097/NezukoBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
